31948714da7c319021d14962a7a632449cb05e3d
spellew <spellew1298@gmail.com>
2019-03-03 01:28:44 -0500

    Added Ship class and enemy group

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,12 +1,13 @@
-const h = window.innerHeight * window.devicePixelRatio;
-const w = h * (16 / 9);
-const s = h / 1775;
-
-
 window.addEventListener("load", () => {
 
+  const h = window.innerHeight * window.devicePixelRatio;
+  const w = h * (16 / 9);
+  const s = h / 1775;
+  const b = 1;
+
   const game = new Phaser.Game(w, h, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update });
-  let ship = null;
+  let player = null;
+  let enemies = null;
 
   function preload() {
 
@@ -17,45 +18,56 @@ window.addEventListener("load", () => {
   
   function create() {
 
-    const b = 9;
-
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     game.add.tileSprite(0, 0, w * b, h * b, 'backdrop');
     game.world.setBounds(0, 0, w * b, h * b);
 
-    ship = game.add.sprite(0, 0, 'ship');
-    ship.anchor.setTo(0.5, 0.5);
-    ship.scale.set(s, s);
-    ship.x = w * b * 0.5;
-    ship.y = h * b * 0.5;
-    game.physics.enable(ship, Phaser.Physics.ARCADE);
-    game.camera.follow(ship);
+    player = new Ship({ player: true });
+    enemies = game.add.group();
     
   }
   
   function update () {
 
-    const speed = 800;
-
-    ship.body.velocity.x = 0;
-    ship.body.velocity.y = 0;
-    ship.body.angularVelocity = 0;
+    player.sprite.body.velocity.x = 0;
+    player.sprite.body.velocity.y = 0;
+    player.sprite.body.angularVelocity = 0;
 
     if (game.input.keyboard.isDown(Phaser.Keyboard.A) || game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-      ship.body.angularVelocity = -400;
+      player.sprite.body.angularVelocity = -400;
     } else if (game.input.keyboard.isDown(Phaser.Keyboard.D) || game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-      ship.body.angularVelocity = 400;
+      player.sprite.body.angularVelocity = 400;
     }
 
     if (game.input.keyboard.isDown(Phaser.Keyboard.W) || game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-      game.physics.arcade.velocityFromAngle(ship.angle - 90, speed, ship.body.velocity);
+      game.physics.arcade.velocityFromAngle(player.sprite.angle - 90, player.speed, player.sprite.body.velocity);
     } else if (game.input.keyboard.isDown(Phaser.Keyboard.S) || game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
-      game.physics.arcade.velocityFromAngle(ship.angle - 90, -speed, ship.body.velocity);
+      game.physics.arcade.velocityFromAngle(player.sprite.angle - 90, -player.speed, player.sprite.body.velocity);
     }
 
-    game.world.wrap(ship, 0, true);
+    game.world.wrap(player.sprite, 0, true);
+    enemies.children.forEach(enemy => game.world.wrap(enemy, 0, true));
 
+    // game.physics.arcade.collide(player.sprite, enemies, () => "overlap");
+
+  }
+
+
+  class Ship {
+
+    constructor({ player, enemy }) {
+      this.sprite = game.add.sprite(0, 0, 'ship');
+      this.sprite.anchor.setTo(0.5, 0.5);
+      this.sprite.scale.set(s, s);
+      this.sprite.x = w * b * 0.5;
+      this.sprite.y = h * b * 0.5;
+      this.health = 100;
+      this.speed = 600;
+      game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
+      player ? game.camera.follow(this.sprite) : enemy ? enemies.add(this.sprite) : null;
+    }
+    
   }
 
 });
